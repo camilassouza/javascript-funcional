3b064a96ec24ca01470847f460ac8c3ef3d859a5
Camila <camila@pop-os.localdomain>
2022-01-17 21:50:10 -0300

    criando um helper em resposta da api

---
+++ project/public/app/app.js
@@ -0,0 +1,11 @@
+import { handleStatus } from "./ultis/promisse-helpers.js"
+
+document
+.querySelector('#myButton')
+.onclick = () => 
+    fetch('http://localhost:3000/notas')
+    .then(handleStatus)
+    .then(notas => console.log(notas))
+    .catch(console.log)
+
+    
